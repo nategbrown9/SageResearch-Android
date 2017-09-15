6d3f56866fac95844da39b9d50eeba6381313147
Joshua Liu <liujoshua@users.noreply.github.com>
2017-09-15 13:38:00 -0700

    Add some javadocs

--- sdk/src/main/java/org/sagebionetworks/research/sdk/AsyncAction.java
+++ sdk/src/main/java/org/sagebionetworks/research/sdk/AsyncAction.java
@@ -20,22 +20,62 @@ package org.sagebionetworks.research.sdk;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+/**
+ * Defines general configuration for asynchronous action that should be run in the background. Depending upon the
+ * parameters and how the action is setup, this could be something that is run continuously or else is paused or reset
+ * based on a timeout interval.
+ */
 public interface AsyncAction {
+    /**
+     * A short string that uniquely identifies the asyncronous action within the task. The identifier is reproduced in
+     * the results of a async results.
+     *
+     * @return identifier
+     */
     @NonNull
     String getIdentifier();
 
+    /**
+     * An identifier marking the step to start the action. If `null`, then the action will be started when the task is
+     * started.
+     *
+     * @return step identifier, or null
+     */
     @Nullable
     String getStartStepIdentifier();
 
+    /**
+     * An identifier marking the step at which to stop the action. If `nil`, then the action will be stopped when the
+     * task is stopped.
+     *
+     * @return step identifier, or null
+     */
     @Nullable
     String getStopStepIdentifier();
 
+    /**
+     * An identifier marking a step to wait to display until the action is completed. This is only valid for actions
+     * that are single result actions and not continuous recorders.
+     *
+     * @return step identifier, or null
+     */
     @Nullable
     String getWaitStepIdentifier();
 
+    /**
+     * A time interval after which the action should be reset. For example, if the action queries a weather service and
+     * the user backgrounds the app for more than the reset time, then the weather service should be queried again.
+     *
+     * @return duration
+     */
     @Nullable
     Duration getResetTimeInterval();
 
+    /**
+     * A time interval after which the action should be stopped.
+     *
+     * @return duration
+     */
     @Nullable
     Duration getTimeoutTimeInterval();
 }
